89a0bf607a211d778998f74f90e55d8a8965afa4
fthomasvp <fellipecab@gmail.com>
2020-07-07 03:56:16 -0300

    Change page background color

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App style={{ background: 'whitesmoke' }} />
   </React.StrictMode>,
   document.getElementById('root')
 );
